757c2ba824a6d56d981f0e95b84b99ce819a8863
SABRETOOTH\simog <simogecko@gmail.com>
2018-08-12 19:06:43 +0200

    worked on kappas

---
+++ Assets/Scripts/Kappa.cs
@@ -0,0 +1,63 @@
+﻿// (c) Simone Guggiari 2018
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+////////// DESCRIPTION //////////
+
+[RequireComponent(typeof(Animator))]
+public class Kappa : MonoBehaviour {
+    // --------------------- VARIABLES ---------------------
+
+    // public
+    public float spawnDelay = .3f;
+
+
+    // private
+    bool spawning;
+
+
+    // references
+    Animator anim;
+	
+	
+	// --------------------- BASE METHODS ------------------
+	void Start () {
+        anim = GetComponent<Animator>();
+
+    }
+	
+	void Update () {
+        
+	}
+
+
+
+    // --------------------- CUSTOM METHODS ----------------
+
+
+    // commands
+    public void StartFalling() {
+        if (spawning) return;
+        anim.SetTrigger("fallout");
+        spawning = true;
+        Invoke("SpawnBox", spawnDelay);
+    }
+
+    public void SpawnBox() {
+        //actual spawn code
+        Box boxPrefab = SpawnManager.instance.GetBoxPrefab();
+        Box box = Instantiate(boxPrefab, transform.position, Quaternion.Euler(0, Random.value * 380, 0)) as Box;
+        spawning = false;
+    }
+
+
+
+    // queries
+
+
+
+    // other
+
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -22,7 +22,7 @@ public class SpawnManager : MonoBehaviour {
     // references
     public static SpawnManager instance;
     public Box[] boxesPrefab;
-    public Transform[] spawnPoints;
+    public Kappa[] kappas;
 
     // --------------------- BASE METHODS ------------------
     private void Awake() {
@@ -44,9 +44,10 @@ public class SpawnManager : MonoBehaviour {
 
     // commands
     void SpawnOne() {
-        int lane = Random.Range(0, spawnPoints.Length);
-        Transform t = spawnPoints[lane];
-        Box box = Instantiate(boxesPrefab[Random.Range(0, boxesPrefab.Length)], t.position, Quaternion.Euler(0, Random.value*380, 0)) as Box;
+        int k = Random.Range(0, kappas.Length);
+        //Transform t = spawnPoints[lane];
+        kappas[k].StartFalling();
+        //Box box = Instantiate(boxesPrefab[Random.Range(0, boxesPrefab.Length)], t.position, Quaternion.Euler(0, Random.value*380, 0)) as Box;
         //set it up
     }
 
@@ -59,6 +60,10 @@ public class SpawnManager : MonoBehaviour {
         float result = 1f / val; // how many seconds waittime
         //Debug.Log("waittime=" + result);
         return result;
+    }
+
+    public Box GetBoxPrefab() {
+        return boxesPrefab[Random.Range(0, boxesPrefab.Length)];
     }
 
 
