b6305f7638657c9a648813b359599fc8d8b5b910
Simone Guggiari <simogecko@gmail.com>
2018-08-26 13:57:34 +0200

    other changes

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -47,30 +47,63 @@ public class InputManager : MonoBehaviour {
 
     // queries
     public Vector2 GetInput(int i=0) {
-        string playerid = (i == 0) ? "" : "2";
-        switch (GameManager.instance.platform) {
-            case GameManager.Platform.Pc:
-                return new Vector2(Input.GetAxis("Horizontal"+ playerid), Input.GetAxis("Vertical"+ playerid));
-            case GameManager.Platform.Mobile:
-                return joystick.InputValue.To3();
-            case GameManager.Platform.Console:
-                return new Vector2(Input.GetAxis("Horizontal"+ playerid), Input.GetAxis("Vertical"+ playerid));
-            default:
-                return Vector2.zero;
+        if (GameManager.instance.Coop) {
+            //single
+            switch (GameManager.instance.platform) {
+                case GameManager.Platform.Pc:
+                    return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+                case GameManager.Platform.Mobile:
+                    return joystick.InputValue.To3();
+                case GameManager.Platform.Console:
+                    return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            }
+        } else {
+            //coop
+            string playerid = (i == 0) ? "" : "2";
+            switch (GameManager.instance.platform) {
+                case GameManager.Platform.Pc:
+                    if(i==0)
+                        return new Vector2(Input.GetAxis("HorizontalAD"), Input.GetAxis("VerticalWS"));
+                    else
+                        return new Vector2(Input.GetAxis("HorizontalLR"), Input.GetAxis("VerticalUD"));
+                case GameManager.Platform.Console:
+                    return new Vector2(Input.GetAxis("Horizontal"+playerid), Input.GetAxis("Vertical"+playerid));
+            }
+
         }
+        return Vector2.zero;
     }
 
     public bool GetInteractInput(int i=0) {
         string playerid = (i == 0) ? "" : "2";
-        switch (GameManager.instance.platform) {
-            case GameManager.Platform.Pc:
-                return Input.GetKeyDown(KeyCode.Space);
-            case GameManager.Platform.Mobile:
-                break;
-            case GameManager.Platform.Console:
-                return Input.GetButtonDown("Interact" + playerid);
-            default: return false;
+        if (!GameManager.instance.Coop) {
+            //single
+            switch (GameManager.instance.platform) {
+                case GameManager.Platform.Pc:
+                    return Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return);
+                case GameManager.Platform.Mobile:
+                    break;
+                case GameManager.Platform.Console:
+                    return Input.GetButtonDown("Interact");
+                default: return false;
+            }
+        } else {
+            //coop
+            switch (GameManager.instance.platform) {
+                case GameManager.Platform.Pc:
+                    if (i == 0)
+                        return Input.GetKeyDown(KeyCode.Space);
+                    else
+                        return Input.GetKeyDown(KeyCode.Return);
+                case GameManager.Platform.Mobile:
+                    break;
+                case GameManager.Platform.Console:
+                    return Input.GetButtonDown("Interact"+playerid);
+                default: return false;
+            }
         }
+
+        
         return false;
     }
 
